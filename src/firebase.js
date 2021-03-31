ac8282768fad4046333382b1865e7ff41253e51d
adam040992 <adam040992@gmail.com>
2021-03-31 19:14:54 +0200

    Creating project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBnNy4qGwC0PdWM7C9SS3b3JOPGgjrUcNs",
+    authDomain: "starbucks-clone-24bc0.firebaseapp.com",
+    projectId: "starbucks-clone-24bc0",
+    storageBucket: "starbucks-clone-24bc0.appspot.com",
+    messagingSenderId: "117067970205",
+    appId: "1:117067970205:web:5a4bff6f90296a9d654674"
+  };
