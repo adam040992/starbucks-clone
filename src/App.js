8f9d418f4e87c9035d9ba57d07e90e112dc7c304
adam040992 <adam040992@gmail.com>
2021-03-31 19:48:23 +0200

    React-router, Material-ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Fade } from 'react-awesome-reveal';
+import HomeScreen from './screens/HomeScreen';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="app">
-      <h1>STARBUCKS</h1>
+      <Router>
+        <Switch>
+          <Route exect path="/">
+            <Header />
+            <HomeScreen />
+            <Fade>
+              <Footer />
+            </Fade>
+          </Route>
+        </Switch>
+    </Router>
     </div>
   );
 }
