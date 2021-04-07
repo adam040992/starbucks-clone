60826383731bb16806378427e951dda8914e54a8
adam040992 <adam040992@gmail.com>
2021-04-07 12:53:38 +0200

    Building header section

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
     return (
-        <div>
-            
+        <div className="header">
+            <div className="header__left">
+                <Link className="header__logo" to="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png" alt="Starbucks_icon" />
+                    
+                </Link>
+                <Link to="">
+                    Menu
+                </Link>
+            </div>
+            <div className="header__right">
+
+            </div>
         </div>
     )
 }
 
 export default Header
+
